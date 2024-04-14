96d33f5c4b2504ae39c7538aace7235b7b3f4656
Muzammal Kazmi <muzammalkazmi@gmail.com>
2024-04-14 15:16:52 +0500

    overview of fuctions and objects

--- objects.ts
+++ objects.ts
@@ -10,3 +10,25 @@ const mycar = new Car('Texla', 2020)
     x:10,
     y:20
  }
+
+ // how to use annotations with objects with nested objects and function 
+
+ const profile ={
+    name: "Ali",
+    age: 20,
+    location:{
+        lag: 0,
+        lat: 13
+    },
+    setAge(age:number):void{
+        this.age = age
+    }
+ };
+
+ // careful about syntax ; the first part is showing destructuring while the second part is of annotation.
+
+ const {age}: {age: number}= profile
+
+ // now destructuring nested object and annotating
+
+ const {location:{lag,lat}}:{location:{lag:number,lat:number}}=profile
